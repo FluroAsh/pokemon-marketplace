f010d322eec10001c4582463aaa7de9c966243b3
FluroAsh <workashleythompson@gmail.com>
2022-03-04 22:34:39 +1100

    adds basic styling to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root "pages#home"
-  get "home", to: "pages#home"
-  get "restricted", to: "pages#restricted"
+  root to: "pages#home"
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "restricted", to: "pages#restricted"
 end
